1e016518c9945a9489a8dd0dcb1f27a00d8b45ef
asdfbn12345 <asdfbn12345@gmail.com>
2025-03-08 07:21:20 +0900

    rename: msw worker 파일 이름 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { worker } from "./mocks/Worker.ts";
 import App from "./App.tsx";
-import { worker } from "./mocks/Browser.ts";
 import "sanitize.css";
 
 switch (import.meta.env.MODE) {
