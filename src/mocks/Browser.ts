e489602ec9de2f94abc52033792e2f6897057202
asdfbn12345 <asdfbn12345@gmail.com>
2025-03-08 11:36:52 +0900

    refactor: msw 코드 리팩토링

--- src/mocks/Browser.ts
+++
@@ -1,7 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { questions } from "./Questions";
-import { categories, categoryImage } from "./Categories";
-
-export const handlers = [categories, categoryImage, questions];
-
-export const worker = setupWorker(...handlers);
