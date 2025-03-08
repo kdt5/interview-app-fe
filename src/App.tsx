333bd12403473e60ad3ffcabf7ae7ba548bc3a1d
asdfbn12345 <83904404+asdfbn12345@users.noreply.github.com>
2025-03-08 19:57:34 +0900

    fix: router import typo (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routers";
+import { router } from "./routers/Router";
 
 function App() {
   return <RouterProvider router={router} />;
