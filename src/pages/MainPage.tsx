a5bf1eac9e4bece33ad18935c904a559b23e29d7
Jung BoYoung <100827373+boyoungg@users.noreply.github.com>
2025-03-08 18:02:05 +0900

    Feature/main page (#2)
    FEAT : 
    - 메인페이지 생성
    - 공통 헤드 컴포넌트 생성
    - 공통 네비 컴포넌트 생성
    
    STYLE: 
    - global 스타일 수정
    
    ---------
    
    Co-authored-by: jooya1009 <rusdnguswn0124@gmail.com>

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,145 @@
+import styled from "styled-components";
+import HomeImg from "../assets/MainPageIcon.png";
+import { SlArrowRight } from "react-icons/sl";
+
+function Home() {
+  return (
+    <>
+      <HomeStyle>
+        <div className="main-title">
+          <h1>
+            이번주 새로운
+            <br />
+            면접질문이 도착했어요
+          </h1>
+          <div>
+            <img src={HomeImg} alt="MainPage Image" />
+          </div>
+        </div>
+        <div className="weekly-question">
+          <p>
+            Javascript에서 var, let, const의 역할과
+            <br />
+            각각의 차이점은 무엇일까요?
+          </p>
+          <div>
+            <span>Front-end</span>
+            <span>Javascript</span>
+          </div>
+        </div>
+
+        <div className="interview-essential-wrap">
+          <div className="contents-title">
+            <span>추천 질문</span>
+            <p>면접 필수 질문</p>
+          </div>
+          <div className="interview-essential">
+            <div className="box">
+              <p>
+                프론트엔드 개발자
+                <br />
+                필수 질문 리스트
+              </p>
+              <span>
+                지금 답변하기 <SlArrowRight />
+              </span>
+            </div>
+            <div className="box">
+              <p>
+                백엔드 개발자
+                <br />
+                필수 질문 리스트
+              </p>
+              <span>
+                지금 답변하기 <SlArrowRight />
+              </span>
+            </div>
+          </div>
+        </div>
+      </HomeStyle>
+    </>
+  );
+}
+
+const HomeStyle = styled.main`
+  width: 100%;
+  padding: 0 30px;
+
+  .main-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .weekly-question {
+    background-color: #6ea1ff;
+    padding: 20px;
+    border-radius: 10px;
+
+    p {
+      color: #fff;
+      width: 100%;
+      margin-bottom: 30px;
+    }
+
+    span {
+      background-color: #bbd3ff;
+      color: #fff;
+
+      font-size: 12px;
+      font-weight: 300;
+
+      border-radius: 15px;
+
+      display: inline-block;
+      margin: 0 5px 0 0;
+      padding: 3px 10px;
+    }
+  }
+
+  .interview-essential-wrap {
+    margin-top: 50px;
+
+    .contents-title {
+      margin-bottom: 10px;
+
+      span {
+        color: #6ea1ff;
+        font-weight: 400;
+      }
+    }
+
+    .interview-essential {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 10px;
+
+      div {
+        width: 50%;
+        padding: 15px;
+
+        p {
+          margin-bottom: 30px;
+        }
+
+        span {
+          font-weight: 400;
+          color: #6ea1ff;
+
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          svg {
+            path {
+              color: #6ea1ff;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Home;
