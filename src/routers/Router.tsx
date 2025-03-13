49b33755b4ac8b20d7f1f0f119330fa41b524bf8
asdfbn12345 <83904404+asdfbn12345@users.noreply.github.com>
2025-03-14 02:56:24 +0900

    fix: react-router-dom 인스턴스 코드 오류 수정, 일부 코드 리팩토링 (#23)
    간단한 코드 오류 수정 작업으로 바로 머지하겠습니다.

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,56 +1,9 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
-import Layout from "../components/layout/Layout";
-import Home from "../pages/MainPage";
-import AnswerPage from "../pages/AnswerPage";
-import LoginPage from "../pages/LoginPage";
-import JoinPage from "../pages/JoinPage";
-import QuestionList from "../pages/QuestionList";
 import React from "react";
-
-
-const routers: RouteObject[] = [
-  {
-    path: "/",
-    element: (
-      <Layout>
-        <Home />
-      </Layout>
-    ),
-  },
-  {
-    path: "/answer",
-    element: (
-      <Layout>
-        <AnswerPage />
-      </Layout>
-    ),
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/join",
-    element: <JoinPage />,
-    path: "/question-list/frontend",
-    element: (
-      <Layout>
-        <QuestionList />
-      </Layout>
-    ),
-  },
-  {
-    path: "/question-list/backend",
-    element: (
-      <Layout>
-        <QuestionList />
-      </Layout>
-    ),
-  },
-];
+import { createBrowserRouter } from "react-router-dom";
+import { routerObjects } from "./RouterObject";
 
 export const router = createBrowserRouter(
-  routers.map((route) => {
+  routerObjects.map((route) => {
     return {
       ...route,
       element: route.element,
