49ff752a267be5a4f8863d63611aab935f41abd0
asdfbn12345 <83904404+asdfbn12345@users.noreply.github.com>
2025-03-14 02:52:25 +0900

    Revert "Fix/react router dom typo" (#22)
    Reverts kdt5/interview-app-fe#21

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { worker } from "./mocks/Worker.ts";
-import App from "./App.tsx";
-import "sanitize.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-switch (import.meta.env.MODE) {
-  case "development":
-    worker.start().then(() => {
-      createReactApp();
-    });
-    break;
-
-  default:
-    createReactApp();
-    break;
-}
-
-function createReactApp() {
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
