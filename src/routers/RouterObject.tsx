7240f408e5c25b6e6088e6d48f19b441244b6fea
asdfbn12345 <asdfbn12345@gmail.com>
2025-03-14 02:49:26 +0900

    fix: react-router-dom 인스턴스 코드 오류 수정, 일부 코드 리팩토링

---
+++ src/routers/RouterObject.tsx
@@ -0,0 +1,39 @@
+import { RouteObject } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import Home from "../pages/MainPage";
+import AnswerPage from "../pages/AnswerPage";
+import LoginPage from "../pages/LoginPage";
+import JoinPage from "../pages/JoinPage";
+import QuestionList from "../pages/QuestionList";
+
+export const routerObjects: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/answer",
+    element: <AnswerPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/join",
+    element: <JoinPage />,
+  },
+  {
+    path: "/question-list/frontend",
+    element: <QuestionList />,
+  },
+  {
+    path: "/question-list/backend",
+    element: <QuestionList />,
+  },
+].map((routerObject) => {
+  return {
+    ...routerObject,
+    element: <Layout>{routerObject.element}</Layout>,
+  };
+});
