1a08cb3244e94e243375ae6dfda32afedcec1d74
jooya1009 <rusdnguswn0124@gmail.com>
2025-03-14 14:21:30 +0900

    FEAT: Answer Page 기능추가 및 수정 (#14)
    - 클래스 네이밍 수정
    - Modal 추상화
    - 스타일 코드를 스타일 컴포넌트 안으로 이동
    - Props 정리

--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -1,99 +1,82 @@
-import { createPortal } from "react-dom";
 import styled from "styled-components";
+import Modal from "./Modal";
 
-interface ConfirmModalProps {
+interface Props {
   onClose: () => void;
   onConfirm: () => void;
 }
 
-function ConfirmModal({ onClose, onConfirm }: ConfirmModalProps) {
-  return createPortal(
-    <ConfirmModalStyle>
-      <div className="ConfirmModalBackground">
+function ConfirmModal({ onClose, onConfirm }: Props) {
+  return (
+    <Modal width="300px" height="150px">
+      <ConfirmModalStyle>
         <form
-          className="ConfirmModal"
+          className="confirm-modal"
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
-          <div className="ConfirmTitle">
-            <p>
+          <div className="confirm-title">
+            <p className="confirm-message">
               제출 하시겠습니까?
               <br />
               제출 후, 수정 및 삭제는 불가능합니다.
             </p>
           </div>
-          <div className="ConfirmButton">
-            <button className="btn1" type="button" onClick={onConfirm}>
+          <div className="confirm-buttons">
+            <button className="ok-button" type="button" onClick={onConfirm}>
               확인
             </button>
-            <div></div>
-            <button className="btn1" type="button" onClick={onClose}>
+            <span className="middle-line"></span>
+            <button className="cancel-button" type="button" onClick={onClose}>
               취소
             </button>
           </div>
         </form>
-      </div>
-    </ConfirmModalStyle>,
-    document.body
+      </ConfirmModalStyle>
+    </Modal>
   );
 }
 
 const ConfirmModalStyle = styled.div`
-  width: 100%;
-  max-width: 380px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 999;
+  .confirm-modal {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background: #ffffff;
+    position: relative;
 
-  .ConfirmModalBackground {
-    width: 380px;
-    height: 100dvh;
-    background: rgba(0, 0, 0, 0.5);
-    padding: 0;
+    .confirm-title {
+      height: 100px;
+      border-bottom: 1px solid #d9d9d9;
+      box-sizing: border-box;
 
-    .ConfirmModal {
-      width: 300px;
-      height: 150px;
-      border-radius: 10px;
-      background: #fff;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
+      .confirm-message {
+        padding-top: 29px;
+        text-align: center;
+      }
+    }
 
-      .ConfirmTitle {
-        height: 100px;
-        border-bottom: 1px solid #d9d9d9;
-        box-sizing: border-box;
-        p {
-          padding-top: 29px;
-          text-align: center;
-        }
+    .confirm-buttons {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .ok-button,
+      .cancel-button {
+        flex-grow: 1;
+        background: none;
+        border: none;
+        color: #333333;
+        height: 50px;
+        cursor: pointer;
+        font-size: 16px;
       }
 
-      .ConfirmButton {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        .btn1 {
-          flex-grow: 1;
-          background: none;
-          border: none;
-          color: #333333;
-          height: 50px;
-          cursor: pointer;
-          font-size: 16px;
-        }
-
-        div {
-          width: 1px;
-          height: 50px;
-          background: #d9d9d9;
-        }
+      .middle-line {
+        width: 1px;
+        height: 50px;
+        background: #d9d9d9;
       }
     }
   }
--- src/pages/AnswerPage.tsx
+++ src/pages/AnswerPage.tsx
@@ -4,63 +4,102 @@ import ConfirmModal from "../components/common/ConfirmModal";
 import { useState } from "react";
 import AlertModal from "../components/common/AlertModal";
 
+type ModalType = "confirm" | "alert";
+
 function AnswerPage() {
-  const [openConfirmModal, setOpenConfirmModal] = useState(false);
-  const [openAlertModal, setOpenAlertModal] = useState(false);
+  const [isModalsVisible, setIsModalsVisible] = useState({
+    confirm: false,
+    alert: false,
+  });
 
-  const handleOpenConfirmModal = () => {
-    setOpenConfirmModal(true);
+  const [answer, setAnswer] = useState("");
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleModal = (type: ModalType, state: boolean) => {
+    setIsModalsVisible((prev) => ({
+      ...prev,
+      [type]: state,
+    }));
   };
 
-  const handleCloseConfirmModal = () => {
-    setOpenConfirmModal(false);
+  const handleSubmit = () => {
+    toggleModal("confirm", true);
   };
 
-  const handleOpenAlertModal = () => {
-    setOpenAlertModal(true);
+  const handleAnswerChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const answerCount = e.target.value;
+
+    if (answerCount.length <= 500) {
+      setAnswer(answerCount);
+    }
   };
 
-  const handlCloseAlertModal = () => {
-    setOpenAlertModal(false);
+  const handleFavoriteCheck = () => {
+    setIsFavorite(!isFavorite);
   };
+
+  const isSubmitDisabled = answer.trim() === "" || answer.length <= 20;
+
+  const isOverLimit = answer.length >= 500;
 
   return (
-    <AnswerPageStyle>
-      <div className="QuestionBox">
-        <div className="QuestionNumbering">
-          <p>01 |</p>
-          <FaStar />
+    <AnswerPageStyle isSubmitDisabled={isSubmitDisabled}>
+      <div className="question-box">
+        <div className="question-numbering">
+          <p className="numbering-title">01 |</p>
+          <FavoriteIcon onClick={handleFavoriteCheck} isFavorite={isFavorite} />
         </div>
-        <h2>JSX에 대해 설명해주세요.</h2>
-        <span>Javascript</span>
+        <h2 className="question-title">JSX에 대해 설명해주세요.</h2>
+        <span className="category-name">Javascript</span>
       </div>
-      <form action="/">
-        <textarea placeholder="답변을 작성해주세요."></textarea>
+      <form action="/" className="answer-box">
+        <textarea
+          placeholder="답변을 작성해주세요."
+          className="answer-text"
+          value={answer}
+          onChange={handleAnswerChange}
+        ></textarea>
+        <p className={`character-count ${isOverLimit ? "over-limit" : ""}`}>
+          {answer.length}/500
+        </p>
       </form>
-      <button className="btn2" type="submit" onClick={handleOpenConfirmModal}>
+      <button
+        className="submit-button"
+        type="submit"
+        onClick={handleSubmit}
+        disabled={isSubmitDisabled}
+      >
         제출
       </button>
-      {openConfirmModal && (
+      {isModalsVisible.confirm && (
         <ConfirmModal
-          onClose={handleCloseConfirmModal}
+          onClose={() => toggleModal("confirm", false)}
           onConfirm={() => {
-            handleCloseConfirmModal();
-            handleOpenAlertModal();
+            toggleModal("confirm", false);
+            toggleModal("alert", true);
           }}
         />
       )}
-      {openAlertModal && <AlertModal onClose={handlCloseAlertModal} />}
+      {isModalsVisible.alert && (
+        <AlertModal onClose={() => toggleModal("alert", false)} />
+      )}
     </AnswerPageStyle>
   );
 }
 
-const AnswerPageStyle = styled.div`
+const FavoriteIcon = styled(FaStar)<{ isFavorite: boolean }>`
+  fill: ${({ isFavorite }) => (isFavorite ? "#FFD600" : "#DFDFDF")};
+  cursor: pointer;
+  font-size: 24px;
+`;
+
+const AnswerPageStyle = styled.div<{ isSubmitDisabled: boolean }>`
   width: 100%;
   max-width: 380px;
   box-sizing: border-box;
   padding: 25px 30px;
 
-  .QuestionBox {
+  .question-box {
     padding: 10px 15px;
     box-sizing: border-box;
     width: 330px;
@@ -68,7 +107,8 @@ const AnswerPageStyle = styled.div`
     border: 1px solid #eff2f8;
     border-radius: 10px;
     background: #fbfbfb;
-    span {
+
+    .category-name {
       margin-top: 40px;
       background-color: #bbd3ff;
       color: #fff;
@@ -80,50 +120,84 @@ const AnswerPageStyle = styled.div`
     }
   }
 
-  .QuestionNumbering {
+  .question-numbering {
     margin-bottom: 10px;
     display: flex;
     justify-content: space-between;
-    p {
+
+    .numbering-title {
       color: #888888;
     }
+
     svg {
       font-size: 24px;
-      fill: #dfdfdf;
-      /* fill: #FFD600; */
       cursor: pointer;
     }
   }
 
-  form {
+  .answer-box {
     border: 1px solid #eff2f8;
     border-radius: 10px;
     width: 330px;
     height: 315px;
     margin: 10px 0;
+
+    .answer-text {
+      -ms-overflow-style: none;
+      width: 100%;
+      height: 100%;
+      padding: 15px;
+      border-radius: 10px;
+      border: none;
+      background: none;
+      resize: none;
+    }
+
+    .answer-text:focus {
+      outline: none;
+    }
+
+    .answer-text::-webkit-scrollbar {
+      display: none;
+    }
+
+    .character-count {
+      font-size: 14px;
+      color: #888;
+      text-align: right;
+    }
+
+    .character-count.over-limit {
+      color: red;
+      animation: shake 0.5s ease-in-out;
+    }
+
+    @keyframes shake {
+      0% {
+        transform: translateX(0);
+      }
+      25% {
+        transform: translateX(-3px);
+      }
+      50% {
+        transform: translateX(3px);
+      }
+      75% {
+        transform: translateX(-3px);
+      }
+      100% {
+        transform: translateX(0);
+      }
+    }
   }
 
-  textarea {
-    -ms-overflow-style: none;
-    width: 100%;
-    height: 100%;
-    padding: 15px;
-    border-radius: 10px;
-    border: none;
-    background: none;
-    resize: none;
-  }
-  textarea:focus {
-    outline: none;
-  }
-  textarea::-webkit-scrollbar {
-    display: none;
-  }
-
-  .btn2 {
+  .submit-button {
     width: 330px;
     height: 60px;
     font-size: 20px;
+    margin-top: 30px;
+    opacity: ${(props) => (props.isSubmitDisabled ? 0.5 : 1)};
+    cursor: ${(props) => (props.isSubmitDisabled ? "not-allowed" : "pointer")};
   }
 `;
 
