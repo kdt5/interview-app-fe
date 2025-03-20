9149c25e0d25d5df36fe4ef730d83fe18a92d3d1
asdfbn12345 <asdfbn12345@gmail.com>
2025-03-20 17:16:33 +0900

    fix: 환경변수 json parse 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "sanitize.css";
 
 switch (import.meta.env.MODE) {
   case "development":
-    if (import.meta.env.VITE_DEVELOPMENT_MSW_MODE === "true") {
+    if (JSON.parse(import.meta.env.VITE_DEVELOPMENT_MSW_MODE) === true) {
       worker.start().then(() => {
         createReactApp();
       });
