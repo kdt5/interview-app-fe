fcb2ceda6bf0ab299d56a328a47f3ae3934d9b96
asdfbn12345 <asdfbn12345@gmail.com>
2025-04-01 01:34:25 +0900

    chore: eslint typescript naming convention setting

--- src/api/BackendHttpClient.api.ts
+++ src/api/BackendHttpClient.api.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// eslint disabled for naming convention because the axios library uses complicated names
 import axios from "axios";
 
 export const backendHttpClient = axios.create({
