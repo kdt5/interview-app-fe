ebfce55cac765b1e69301dd98e6741994fe3af93
Jung BoYoung <100827373+boyoungg@users.noreply.github.com>
2025-04-04 02:32:19 +0900

    feat: CommunityList에서 CommonProfile 분리

--- src/components/common/List/CommunityList.tsx
+++ src/components/common/List/CommunityList.tsx
@@ -1,41 +1,29 @@
 import styled from "styled-components";
+import CommonProfile from "../Profile/CommonProfile";
 
 interface Props {
-  profileImg: string;
-  username: string;
-  comments: number;
-  level: number;
   title: string;
   content: string;
   views: number;
   likes: number;
 }
 
-function CommunityList({
-  profileImg,
-  username,
-  comments,
-  level,
-  title,
-  content,
-  views,
-  likes,
-}: Props) {
+const ProfileData = [
+  {
+    profileImg: "https://via.placeholder.com/40",
+    username: "내가말하고있잖아",
+    comments: 25,
+    level: 5,
+  },
+];
+
+function CommunityList({ title, content, views, likes }: Props) {
   return (
     <>
       <CommunityListStyle>
-        <ProfileSection>
-          <CommonProfile src={profileImg} alt={`${username}의 프로필`} />
-          <FlexWrap>
-            <UserInfo>
-              <Username>{username}</Username>
-              <Comments>누적 답변{comments}개</Comments>
-            </UserInfo>
-            <Level>
-              LV. <b>{level}</b>
-            </Level>
-          </FlexWrap>
-        </ProfileSection>
+        {ProfileData.map((item, index) => (
+          <CommonProfile key={index} {...item} />
+        ))}
         <Title>{title}</Title>
         <Content>{content}</Content>
         <Info>
@@ -50,57 +38,6 @@ const CommunityListStyle = styled.div`
   border-bottom: 3px solid #f5f5f5;
   cursor: pointer;
   padding: 25px 30px;
-`;
-
-const CommonProfile = styled.img`
-  width: 35px;
-  height: 35px;
-  background-color: #ccc;
-  border-radius: 30px;
-  display: block;
-`;
-
-const ProfileSection = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: left;
-  margin-bottom: 10px;
-`;
-
-const FlexWrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: calc(100% - 35px);
-`;
-
-const UserInfo = styled.div`
-  margin-left: 10px;
-`;
-
-const Username = styled.div`
-  font-weight: 600;
-  font-size: 14px;
-  color: #333;
-`;
-
-const Comments = styled.div`
-  font-weight: 300;
-  font-size: 12px;
-  color: #888888;
-`;
-
-const Level = styled.div`
-  font-size: 12px;
-  color: #fff;
-  background-color: #6ea1ff;
-  border-radius: 20px;
-  font-weight: 100;
-  padding: 5px 10px;
-
-  b {
-    color: #fff;
-  }
 `;
 
 const Title = styled.h3`
