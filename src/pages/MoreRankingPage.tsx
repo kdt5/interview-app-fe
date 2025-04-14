8d1796b14222ef040f123f855b651738146dc33e
Jooya <rusdnguswn0124@gmail.com>
2025-04-14 11:08:57 +0900

    FEAT: Create MoreRankingPage.tsx

---
+++ src/pages/MoreRankingPage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+function MoreRankingPage() {
+  return <h1>MoreRankingPage</h1>;
+}
+
+export const MoreRankingPageStyle = styled.div``;
+
+export default MoreRankingPage;
