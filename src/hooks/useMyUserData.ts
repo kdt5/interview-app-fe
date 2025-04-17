9f25449131db851ba725593eb2fb23cd1adbcf2a
LikeCandy <blue-flames@naver.com>
2025-04-17 16:21:28 +0900

    refactor/hook: useMyUserData

--- src/hooks/useMyUserData.ts
+++ src/hooks/useMyUserData.ts
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchMyUserData } from "../api/User.api";
 
-export const useMyUserData = () => {
+export function useMyUserData() {
   return useQuery({
     queryKey: ["myUserData"],
     queryFn: fetchMyUserData,
     staleTime: 1000 * 60 * 5,
   });
-};
+}
