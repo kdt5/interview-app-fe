9392c9013dd441efdcc70c0825b396bba8f1d4fb
FeelsBotMan <87961262+FeelsBotMan@users.noreply.github.com>
2025-04-18 21:56:25 +0900

    Feature/user api (#93)
    수정 및 추가된 타입
    - [x] UserBasicInfo
    - [x] UserStats 
    
    수정 및 추가된 api
    - [x] refreshToken
    - [x] fetchUserStats
    - [x] recoverPassword
    - [x] resetPassword
    - [x] changeNickname
    - [x] changePassword

--- src/api/User.api.ts
+++ src/api/User.api.ts
@@ -1,11 +1,51 @@
+import { HttpStatusCode } from "axios";
 import { BACKEND_URLS } from "../constants/Urls";
-import { UserBasicInfo } from "../models/User.model";
+import { UserBasicInfo, UserStats } from "../models/User.model";
 import { backendHttpClient } from "./BackendHttpClient.api";
 
 export async function fetchMyUserData(): Promise<UserBasicInfo> {
   const response = await backendHttpClient
     .get<UserBasicInfo>(BACKEND_URLS.USERS.ME)
     .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+
+  return response;
+}
+
+export async function fetchUserStats(): Promise<UserStats> {
+  const response = await backendHttpClient
+    .get<UserStats>(BACKEND_URLS.USERS.STATS)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+
+  return response;
+}
+
+export async function changeNickname(nickname: string): Promise<boolean> {
+  const response = await backendHttpClient
+    .post(BACKEND_URLS.USERS.CHANGE_NICKNAME, { nickname })
+    .then((response) => response.status === HttpStatusCode.Ok)
+    .catch((error) => {
+      throw error;
+    });
+
+  return response;
+}
+
+export async function changePassword(
+  oldPassword: string,
+  newPassword: string
+): Promise<boolean> {
+  if (!oldPassword || !newPassword) {
+    throw new Error("Missing required parameters");
+  }
+  const response = await backendHttpClient
+    .post(BACKEND_URLS.USERS.CHANGE_PASSWORD, { oldPassword, newPassword })
+    .then((response) => response.status === HttpStatusCode.Ok)
     .catch((error) => {
       throw error;
     });
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -5,5 +5,13 @@ export interface UserBasicInfo {
   _count: {
     answers: number;
   };
+  positionId: number;
   profileImageUrl?: string;
+}
+
+export interface UserStats {
+  answerCount: number; // 답변 수
+  favoriteCount: number; // 받은 좋아요 합계
+  communityPostCount: number; // 커뮤니티 게시글 수
+  commentCount: number; // 댓글 수
 }
