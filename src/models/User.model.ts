e32d59961a85f3e90ac1dc2f55d1662e67a6e88b
FeelsBotMan <sjb0229@gmail.com>
2025-04-18 18:05:12 +0900

    chore: USER 모델의 통계 항목에 대한 주석 추가

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -10,8 +10,8 @@ export interface UserBasicInfo {
 }
 
 export interface UserStats {
-  answerCount: number;
-  favoriteCount: number;
-  communityPostCount: number;
-  commentCount: number;
+  answerCount: number; // 답변 수
+  favoriteCount: number; // 받은 좋아요 합계
+  communityPostCount: number; // 커뮤니티 게시글 수
+  commentCount: number; // 댓글 수
 }
