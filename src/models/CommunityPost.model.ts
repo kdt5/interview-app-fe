f253a30a551d051b54eb23224ccd04de603ed27a
Jade Kim <csia19030@gmail.com>
2025-04-22 15:29:44 +0900

    chore: add postCategoryId field to communityPost model

--- src/models/CommunityPost.model.ts
+++ src/models/CommunityPost.model.ts
@@ -2,12 +2,13 @@ export interface CommunityPost {
     id: number;
     title: string;
     content: string;
+    postCategoryId: number;
     user: {
         id: number;
         nickname: string;
         profileImageUrl: string;
-        answerCount: number;
         level: number;
+        answerCount: number;
     }
     createdAt: string;
     updatedAt: string;
