0029e273d5362d6cac06fd52d34eb4e684a58de7
Jade Kim <csia19030@gmail.com>
2025-04-23 17:08:37 +0900

    feat: refactor ui for comment reply page and change reply count to show total subcomment count

--- src/pages/Community/CommunityReply.tsx
+++ src/pages/Community/CommunityReply.tsx
@@ -1,37 +1,63 @@
 import styled from "styled-components";
-import CommentContents from "../../components/common/Comment";
+import CommentContents, { CommentInfo, Comments, CommonProfileStyle, Contents, FlexWrap, OptionButton, ProfileSection, UserInfo, Username } from "../../components/common/Comment";
 import TextArea from "../../components/common/Community/Textarea";
-import ReplyInfo from "../../components/common/Community/ReplyInfo";
+import { useCommunityPostComments } from "../../hooks/UsePost";
+import { useParams } from "react-router-dom";
+import LikeSmall from "../../assets/Link_Small.png";
+import ReplySmall from "../../assets/Reply_Small.png";
+import { countAllReplies } from "../../utils/commentCount";
 
-const CommentData = [
-  {
-    profileImg: "프로필이미지",
-    username: "닉네임",
-    contents: "답변 챗지피티가 달아준거 티나염",
-    comments: 24,
-    likes: 52,
-    reply: 12,
-    totalcomments: 24,
-  },
-];
-
-const relpyinfo = [
-  {
-    totalcomments: 24,
-  },
-];
 function CommunityReply() {
+  const { postId, commentId } = useParams();
+  const { communityPostComments } = useCommunityPostComments(Number(postId), "post");
+  const subComment = communityPostComments?.find((comment) => comment.id === Number(commentId));
+  const subCommentTopLevelComments = communityPostComments?.filter((comment) => comment.parentId === Number(commentId)) || [];
+  const getReplies = (parentId: number) => (communityPostComments?.filter((comment) => comment.parentId === parentId) || []);
+
   return (
     <>
-      {relpyinfo.map((item, index) => (
-        <ReplyInfo key={index} {...item} />
-      ))}
-      <CommunityReplyStyle>
-        {CommentData.map((item, index) => (
-          <CommentContents key={index} {...item} />
-        ))}
-      </CommunityReplyStyle>
-      <TextArea></TextArea>
+      <ProfileSection>
+        <FlexWrap>
+            <UserInfo>
+              <CommonProfileStyle src={subComment?.user.profileImageUrl} alt={`${subComment?.user.nickname}의 프로필`} />
+              <Username>
+                <p>{subComment?.user.nickname}</p>
+                <Comments>누적 답변{subComment?.user.answerCount}개</Comments>
+              </Username>
+            </UserInfo>
+            <OptionButton></OptionButton>
+        </FlexWrap>
+        <Contents>{subComment?.content}</Contents>
+        <CommentInfo>
+          <span>
+            <img src={LikeSmall} alt="" />
+            좋아요 {subComment?.favoriteCount}
+          </span>{" "}
+          <span>
+            <img src={ReplySmall} alt="" />
+            답글 {countAllReplies(communityPostComments, Number(commentId))}
+          </span>
+        </CommentInfo>
+        {
+          subCommentTopLevelComments ? (
+            (subCommentTopLevelComments.length > 0) ? (
+              <>
+              <CommunityReplyStyle>
+                {subCommentTopLevelComments.map((item) => (
+                  <CommentContents key={item.id} {...item} replies={getReplies(item.id)} depth={0} postId={Number(postId)} allComments={communityPostComments}/>
+                ))}
+              </CommunityReplyStyle>
+            </>
+            ) : (
+              <div></div>
+            )
+            
+          ) : (
+            <div>로딩중...</div>
+          )
+        }
+        <TextArea></TextArea>
+      </ProfileSection>
     </>
   );
 }
