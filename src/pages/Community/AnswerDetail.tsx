db2c42e5e22b34d2a58fbc923c06d008d3480f71
Jade Kim <csia19030@gmail.com>
2025-04-29 16:37:28 +0900

    chore: create page for answer detail

---
+++ src/pages/Community/AnswerDetail.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import CommunityAnswer from "../../components/Community/CommunityAnswer";
+import ReplyInfo from "../../components/common/Community/ReplyInfo";
+import { useParams } from "react-router-dom";
+import { useFetchAnswer } from "../../hooks/UseFetchAnswers";
+
+function AnswerDetail() {
+  const { answerId } = useParams() as { answerId: string };
+  const { answer } = useFetchAnswer(parseInt(answerId));
+
+  if(!answer) return null;
+
+  return (
+    <>
+      <AnswerDetailStyle>
+        <CommunityAnswer key={answer.id}
+          className="interview" 
+          title={answer.question.title}
+          postCategoryId={answer.question.categories[0].category.id}
+          {...answer}
+        />
+      </AnswerDetailStyle>
+      <ReplyInfo totalComments={0} />
+    </>
+  );
+}
+
+const AnswerDetailStyle = styled.div`
+  padding: 0 30px 20px;
+  border-bottom: 5px solid #f5f5f5;
+`;
+
+export const CommentStyle = styled.div`
+  margin-top: 30px;
+  padding: 0 30px;
+`;
+
+export default AnswerDetail;
